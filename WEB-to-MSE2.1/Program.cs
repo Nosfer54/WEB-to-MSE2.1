e431de9c3056c66fea5bec8ce808411b53951b25
Alexandr Kharin <alexandr.kharin@skbkontur.ru>
2018-09-03 12:00:44 +0700

    Перевел алгоритм на отдельные фунции. Прописываю базовые функции извлечения полей для карты

--- WEB-to-MSE2.1/Program.cs
+++ WEB-to-MSE2.1/Program.cs
@@ -5,16 +5,33 @@ using System.Text;
 using System.Threading.Tasks;
 using HtmlAgilityPack;
 using System.Net;
+using System.Text.RegularExpressions;
 
 namespace WEB_to_MSE2._1
 {
     public class Card
     {
-        public string id, href, Name, Cost, Img, Type, NumberOfSet, Stats, Rarity, NameENG, stylesheet, FileImg, Color,
-                        RuleText, FlavorText, loyalty, Illustrator, Power, Toughness, TextBoxes, NumberCoordinates, DividerCoordinates, SpecialText;
+        public string   Name,
+                        Cost,
+                        Type,
+                        TextOracle,
+                        TextFlavor,
+
+            id, href,   Img,  NumberOfSet, Stats, Rarity, NameENG, stylesheet, FileImg, Color,
+                          loyalty, Illustrator, Power, Toughness, TextBoxes, NumberCoordinates, DividerCoordinates, SpecialText;
         public Int32 Number;
         public List<string> LoyaltyCost = new List<string>();
         public List<string> LevelText = new List<string>();
+
+        public void Print()
+        {
+            Console.WriteLine("Имя: " + Name);
+            Console.WriteLine("Стоимость: " + Cost);
+            Console.WriteLine("Тип: " + Type);
+            Console.WriteLine("Oracle текст: " + TextOracle);
+            Console.WriteLine("Художественный текст: " + TextFlavor);
+            Console.WriteLine("");
+        }
     }
 
     class Program
@@ -76,24 +93,79 @@ namespace WEB_to_MSE2._1
 
         }
 
+        public static string Name(HtmlDocument htmlDocument)
+        {
+            string Name;
+            Name = htmlDocument.DocumentNode.SelectSingleNode("//h1[@class='card-text-title']").InnerHtml.Trim();
+            return Regex.Split(Name, "<span")[0].Trim();
+        }
+
+        public static string Cost(HtmlDocument htmlDocument)
+        {
+            string Cost = "";
+
+            if (htmlDocument.DocumentNode.SelectSingleNode("//h1[@class='card-text-title']/span[@class='card-text-mana-cost']") != null)
+            {
+                Cost = htmlDocument.DocumentNode.SelectSingleNode("//h1[@class='card-text-title']/span[@class='card-text-mana-cost']").InnerText.Trim();
+                Cost = Cost.Replace("{", null);
+                Cost = Cost.Replace("}", null);
+            }
+            else
+            {
+                Cost = null;
+            }
+            
+            return Cost;
+        }
+        
+        public static string Type (HtmlDocument htmlDocument)
+        {
+            return htmlDocument.DocumentNode.SelectSingleNode("//p[@class='card-text-type-line']").InnerText.Trim();
+        }
+
+        public static string TextOracle(HtmlDocument htmlDocument)
+        {
+            string Text = htmlDocument.DocumentNode.SelectSingleNode("//div[@class='card-text-oracle']").InnerText.Trim();
+            Text = Text.Replace("{", "<sym>");
+            Text = Text.Replace("}", "</sym>");
+            return Text;
+        }
+
+        public static string TextFlavor(HtmlDocument htmlDocument)
+        {
+            if (htmlDocument.DocumentNode.SelectSingleNode("//div[@class='card-text-flavor']") != null)
+            {
+                return htmlDocument.DocumentNode.SelectSingleNode("//div[@class='card-text-flavor']").InnerText.Trim();
+            } else
+            {
+                return null;
+            }
+            
+        }
+
         public static Card GetRuCard(string href)
         {
             HtmlDocument htmlDocument = new HtmlDocument();
-            var aa = new WebClient();
-            aa.Encoding = Encoding.UTF8;
-            htmlDocument.LoadHtml(aa.DownloadString(href));
+            var webClient = new WebClient();
+            webClient.Encoding = Encoding.UTF8;
+            htmlDocument.LoadHtml(webClient.DownloadString(href));
             var Cards = new Card();
 
-            Cards.Name = htmlDocument.DocumentNode.SelectSingleNode("//h1[@class='card-text-title']").InnerText;
-            Cards.id = "dfsdfsfsdfsdfsd";
+            Cards.Name          = Name(htmlDocument);
+            Cards.Cost          = Cost(htmlDocument); 
+            Cards.Type          = Type(htmlDocument);
+            Cards.TextOracle    = TextOracle(htmlDocument);
+            Cards.TextFlavor    = TextFlavor(htmlDocument);
 
             return Cards;
         }
 
         static void Main(string[] args)
         {
-            //Console.WriteLine(GetRuCard("https://scryfall.com/card/hou/1/ru/%D0%B3%D0%B5%D1%80%D0%BE%D0%B8%D1%87%D0%B5%D1%81%D0%BA%D0%B8%D0%B9-%D0%BF%D0%BE%D0%B4%D0%B2%D0%B8%D0%B3"));
-            Console.WriteLine(GetRuCard("https://scryfall.com/card/hou/1/ru/%D0%B3%D0%B5%D1%80%D0%BE%D0%B8%D1%87%D0%B5%D1%81%D0%BA%D0%B8%D0%B9-%D0%BF%D0%BE%D0%B4%D0%B2%D0%B8%D0%B3").Name);
+            GetRuCard("https://scryfall.com/card/hou/56/ru/%D0%BF%D1%80%D0%BE%D0%BA%D0%BB%D1%8F%D1%82%D0%B0%D1%8F-%D0%BE%D1%80%D0%B4%D0%B0").Print();
+            GetRuCard("https://scryfall.com/card/hou/1/ru/%D0%B3%D0%B5%D1%80%D0%BE%D0%B8%D1%87%D0%B5%D1%81%D0%BA%D0%B8%D0%B9-%D0%BF%D0%BE%D0%B4%D0%B2%D0%B8%D0%B3").Print();
+            GetRuCard("https://scryfall.com/card/hou/165/ru/%D0%B7%D0%B5%D1%80%D0%BA%D0%B0%D0%BB%D0%BE-%D0%BC%D0%B8%D1%80%D0%B0%D0%B6%D0%B5%D0%B9").Print();
+            GetRuCard("https://scryfall.com/card/hou/195/ru/%D0%B1%D0%BE%D0%BB%D0%BE%D1%82%D0%BE").Print();
         }
     }
 }
